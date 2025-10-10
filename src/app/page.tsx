06b2199e45868c079f64c3e414942fc6d2bf8d24
Your Full Name <timobest121@gmail.com>
2025-10-10 13:37:52 +0100

    edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
               <h1 className="text-[32px] md:text-[40px] font-bold">
                 INTEREST TEAMS
               </h1>
-              <p className="text-[20px] max-w-[700] text-start md:text-center">
+              <p className="text-[18px] max-w-[700] text-start md:text-center">
                 Looking for opportunities to grow and serve God? We have
                 different group and units in the Ignited youth that you can join
               </p>
@@ -36,7 +36,7 @@ export default function Home() {
           <Container className="flex gap-6 flex-col lg:flex-row lg:gap-14 items-start justify-between">
             <div className="w-full gap-4  md:min-w-[400px] md:gap-8 md:basis-[30%] flex flex-col items-start">
               <div className="px-4 py-3 border-2 border-purple-800 rounded-full max-w-[280px]">
-                <p className="text-md text-purple-800">
+                <p className="text-[16px] text-purple-800">
                   Answers to Your Questions
                 </p>
               </div>
--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -22,19 +22,19 @@ function Faq() {
               </p>
             </AccordionTrigger>
             <AccordionContent>
-              <p className="text-[18px]">
+              <p className="text-[16px]">
                 Our usual Sunday Service is usually as follow
               </p>
               <div className="flex flex-col gap-1 mt-2">
-                <p className="text-[18px] font-semibold">
+                <p className="text-[16px] font-semibold">
                   - First Service: 9:15am - 11:00am.
                 </p>
 
-                <p className="text-[18px] font-semibold">
+                <p className="text-[16px] font-semibold">
                   - Sunday School: <span>11:00am - 11:45am</span>
                 </p>
 
-                <p className="text-[18px] font-semibold">
+                <p className="text-[16px] font-semibold">
                   - Sunday Service: 11:45am - 1:15am.
                 </p>
               </div>
@@ -55,7 +55,7 @@ function Faq() {
               </p>
             </AccordionTrigger>
             <AccordionContent>
-              <p className="text-[18px]">
+              <p className="text-[16px]">
                 Absolutely!. We're a mission-minded community passionate about
                 walking people on their faith joirney. Reach out to us via the
                 visit us page and someone from our tream will be glad to connect
@@ -78,7 +78,7 @@ function Faq() {
               </p>
             </AccordionTrigger>
             <AccordionContent>
-              <p className="text-[18px]">
+              <p className="text-[16px]">
                 Whether you're a teenager, married, single, or over 30, there's
                 a place for you here! We have vibrant communities for different
                 life stages and interests. Head over to our Communuties Page to
@@ -101,22 +101,22 @@ function Faq() {
               </p>
             </AccordionTrigger>
             <AccordionContent>
-              <p className="text-[18px] ">
+              <p className="text-[16px] ">
                 Here at ignited youth, we are Christ-centered. Word-focused
                 church with a strong emphasis on evangelism and spiritual
                 growth. We believe:
               </p>
               <div className="flex flex-col gap-1 mt-2">
-                <p className="text-[18px]">
+                <p className="text-[16px]">
                   - The Bible is God's infallible word and our ultimate guide.
                 </p>
 
-                <p className="text-[18px]">
+                <p className="text-[16px]">
                   - Jesus Christ is the Son of God, who dies for our sins and
                   rose again for our salvation.
                 </p>
 
-                <p className="text-[18px]">
+                <p className="text-[16px]">
                   - Every believer is empowered by the Holy Spirit to live a
                   victorious life.
                 </p>
